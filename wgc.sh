95cfb18417ebd9be10f337e532d25661ec7054d2
jsav0 <jas.savage@gmail.com>
2020-04-06 06:32:00 -0400

    removed comments

--- wgc.sh
+++ wgc.sh
@@ -2,11 +2,11 @@
 #
 # n0
 # File: wgc.sh
-# Description: Create wireguard tunnels and add/remove peers 
-# Written: jsavage [20200316]
+# Description: Create wireguard server/client configs and add/remove peers 
+# Written: jsavage [updated: 20200406]
 # a110w
 
-SERVER_PUBLIC_IP="104.139.42.88"
+SERVER_PUBLIC_IP=""
 
 usage() {
 echo "WireGuardConfigure:
@@ -14,29 +14,27 @@ USAGE:
   ./wgc.sh [DOWHAT] [OPTIONS]
 
 DOWHAT:
-  create	create wireguard interface (server config)
+  create	create wireguard tunnel interface (server)
   add-peer	create/add peer and generate client config
   remove-peer	remove peer from wireguard interface
 
 OPTIONS:
   <interface>	wireguard interface
-  <ip>		new wireguard tunnel address
   <peer-name>	alias for new client/peer
 
   EXAMPLES:
-  ./wgc.sh create <interface> <ip>
+  ./wgc.sh create <interface>
   ./wgc.sh add-peer <interface> <peer-name>
   ./wgc.sh remove-peer <interface> <peer-pubkey>
 
-  ./wgc.sh create wg0 10.0.0.1 
-  ./wgc.sh add-peer wg0 thinkpadx220
+  ./wgc.sh create wg0
+  ./wgc.sh add-peer wg0 thinkpad-client1
   ./wgc.sh remove-peer wg0 abcde123=
 "
 }
 
 # create new wireguard interface
 add_interface() {
-#  VPN_IP="$2"
   INTFC="$1"
   SERVER_PRIVKEY=~/.config/wireguard/$INTFC-privkey
   SERVER_PUBKEY=~/.config/wireguard/$INTFC-pubkey
@@ -44,30 +42,19 @@ add_interface() {
 	mkdir -p ~/.config/wireguard
   	gen_server_keys
   }
-cat templates/server.conf | sed 's|:SERVER_PRIVKEY:|'"$(cat $SERVER_PRIVKEY)"'|' > /etc/wireguard/"$INTFC".conf
+  cat templates/server.conf | sed 's|:SERVER_PRIVKEY:|'"$(cat $SERVER_PRIVKEY)"'|' > /etc/wireguard/"$INTFC".conf
   wg-quick up "$INTFC"
-#  ip link add $INTFC type wireguard 
-#  ip addr add $VPN_IP dev $INTFC
-#  wg set $INTFC private-key $SERVER_PRIVKEY
-#  ip link set $INTFC up
-#  wg show $INTFC
 }
 
-# generate keys for interface
 gen_server_keys() {
   umask 0777 && wg genkey | tee $SERVER_PRIVKEY | wg pubkey > $SERVER_PUBKEY
 }
 
-# remove peer from interface
 remove_peer() {
   wg set "$1" peer "$2" remove
   wg show
 }
 
-#INTFC=$2
-#SERVER_PUBKEY=$(cat ~/.config/wireguard/$INTFC-pubkey)
-#echo $INTFC
-#echo $SERVER_PUBKEY
 get_next_ip() {
   [ ! -f last-ip.txt ] && printf "10.0.0.1" > last-ip.txt
   ip="10.0.0."$(expr $(cat last-ip.txt | awk -F'.' '{print $4}') + 1)
@@ -98,17 +85,17 @@ create_client_config() {
 }
 
 case "$1" in 
-    create)         add_interface "$2" "$3"
-                    ;;
-    add-peer)		create_client_config "$3" && {
-						add_client_peer "$2" "$3"  && {
-							printf "$ip $NAME\n" | sudo tee -a /etc/hosts
-						} || {
-							echo "error: failed to add peer to wireguard interface"
-							exit 1
-						}
-					}
-					;;
+    create)	add_interface "$2" "$3"
+	    	;;
+    add-peer)	create_client_config "$3" && {
+			add_client_peer "$2" "$3"  && {
+				printf "$ip $NAME\n" | sudo tee -a /etc/hosts
+			} || {
+				echo "error: failed to add peer to wireguard interface"
+				exit 1
+			}
+		}
+		;;
     remove-peer)    remove_peer "$2" "$3"
                     ;;
     *)              usage
